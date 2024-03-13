c25664c40bcc4c554147652f973916828a05492a
Umut ABALI <ua63aba@outlook.com>
2024-03-13 18:53:21 +0300

    Pandas Coding

--- NumPy/My-NumPy-Works.py
+++ NumPy/My-NumPy-Works.py
@@ -1,4 +1,16 @@
 import numpy as np
 
+
+#1. DataTypes and attributes
+
 a1 = np.array([1, 2, 3])
 print(a1)
+print(f"Matris Boyut:{a1.shape}")
+print(f"Türü:{a1.dtype}")
+print(f"Dizi boyutu:{a1.ndim} \n")
+a2 = np.array([[2,3],[5,6]])
+print(a2)
+print(f"Matris Boyut:{a2.shape}")
+print(f"Türü:{a2.dtype}")
+print(f"Dizi boyutu:{a2.ndim}")
+
